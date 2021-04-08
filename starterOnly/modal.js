35e472723fb36783bfecce6059de6bf387818584
Rachid <rachidboubekeur@free.fr>
2021-04-08 10:56:35 +0200

    feat(formSign): ajout de la fonction fermeture de la modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,23 +1,33 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+  let topNav = document.getElementById("myTopnav");
+  if (topNav.className === "topnav") {
+    topNav.className += " responsive";
   } else {
-    x.className = "topnav";
+    topNav.className = "topnav";
   }
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
+const modalBg = document.querySelector(".bground");
+const close = document.querySelector(".close");
+const content = document.querySelector(".content");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+close.addEventListener("click", closeModal);
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+  modalBg.style.display = "block";
+  content.style.animationName = "modalopen";
+}
+
+// close modal form
+function closeModal() {
+  content.style.animationName = "modalclose";
+  setTimeout(function () { modalBg.style.display = "none"; }, 725);
 }
 
 
