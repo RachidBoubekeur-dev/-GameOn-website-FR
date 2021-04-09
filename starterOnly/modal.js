097c49028d3f19b966c336390f3cfe368522bc38
Rachid <rachidboubekeur@free.fr>
2021-04-09 11:36:45 +0200

    docs(modal.js): Ajouts de commentaires

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -53,52 +53,75 @@ function closeModal() {
 submitForm.addEventListener('submit', function (event) {
   event.preventDefault();
 
+  // control input first
   if (firstInputForm.value.length >= 2 && regexAlpha.test(firstInputForm.value)) {
     firstInputForm.style.border = "0px solid red";
     document.querySelector("#errorForm1").style.display = "none";
+
+    // control input last
     if (lastInputForm.value.length >= 2 && regexAlpha.test(lastInputForm.value)) {
       lastInputForm.style.border = "0px solid red";
       document.querySelector("#errorForm2").style.display = "none";
+
+      // control input email
       if (emailInputForm.validity.valid) {
         emailInputForm.style.border = "0px solid red";
         document.querySelector("#errorForm3").style.display = "none";
+
+        // control input birthdate
         if (birthdateInputForm.value.length >= 6) {
           birthdateInputForm.style.border = "0px solid red";
           document.querySelector("#errorForm4").style.display = "none";
+
+          // control input quantity
           if (!isNaN(quantityInputForm.value) && quantityInputForm.value >= 0 && quantityInputForm.value <= 99) {
             quantityInputForm.style.border = "0px solid red";
             document.querySelector("#errorForm5").style.display = "none";
+
+            // control input location city
             if (location1InputForm.checked === true || location2InputForm.checked === true || location3InputForm.checked === true || location4InputForm.checked === true || location5InputForm.checked === true || location6InputForm.checked === true) {
               document.querySelector("#errorForm6").style.display = "none";
+
+              // control input checkbox1
               if (checkbox1InputForm.checked === true) {
+
+                // if form is valid
                 document.querySelector("#errorForm7").style.display = "none";
                 document.querySelector("form").style.display = "none";
                 modalBody.style.display = "flex";
                 modalBody.style.height = "26em";
                 ValideForm.style.display = "block";
+
               } else {
+                // error form input checkbox1
                 document.querySelector("#errorForm7").style.display = "block";
               }
             } else {
+              // error form input location city
               document.querySelector("#errorForm6").style.display = "block";
             }
           } else {
+            // error form input quantity
             quantityInputForm.style.border = "1px solid red";
             document.querySelector("#errorForm5").style.display = "block";
           }
         } else {
+          // error form input birthdate
           birthdateInputForm.style.border = "1px solid red";
           document.querySelector("#errorForm4").style.display = "block";
         }
       } else {
+        // error form input email
         emailInputForm.style.border = "1px solid red";
         document.querySelector("#errorForm3").style.display = "block";
       }
     } else {
+      // error form input last
       lastInputForm.style.border = "1px solid red";
       document.querySelector("#errorForm2").style.display = "block";
     }
   } else {
+    // error form input first
     firstInputForm.style.border = "1px solid red";
     document.querySelector("#errorForm1").style.display = "block";
   }
